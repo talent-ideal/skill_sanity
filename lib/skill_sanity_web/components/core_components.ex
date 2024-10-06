ef2c8b92bf4d005e2df3044e6a67fe723fae90ec
Pierre Cavin <me@sherlox.io>
2024-10-06 06:13:51 +0200

    fix: fix Gettext deprecation warning

--- lib/skill_sanity_web/components/core_components.ex
+++ lib/skill_sanity_web/components/core_components.ex
@@ -15,9 +15,9 @@ defmodule SkillSanityWeb.CoreComponents do
   Icons are provided by [heroicons](https://heroicons.com). See `icon/1` for usage.
   """
   use Phoenix.Component
+  use Gettext, backend: SkillSanityWeb.Gettext
 
   alias Phoenix.LiveView.JS
-  import SkillSanityWeb.Gettext
 
   @doc """
   Renders a modal.
